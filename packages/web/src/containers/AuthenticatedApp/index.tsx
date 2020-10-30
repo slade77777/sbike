1b8296d7ca888154e348e213e0a8ad1812612076
Chris Ho <dung.ho@onemount.com>
2020-10-30 07:18:01 +0700

    add layout

--- packages/web/src/containers/AuthenticatedApp/index.tsx
+++ packages/web/src/containers/AuthenticatedApp/index.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import React, {useState} from 'react';
+import './styles.css';
+import {Layout, Menu} from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import Account from '../Account';
-import Content from './Content';
+
+const {Header, Sider, Content} = Layout;
 
 const AuthenticatedApp = () => {
+  const [collapsed, setCollapsed] = useState(false);
   return (
     <BrowserRouter>
-      <>
-        <Header username="hello" />
-        <Switch>
-          <Route path="/second" exact>
-            <Account />
-          </Route>
-
-          <Route path="/" exact>
-            <Content />
-          </Route>
-        </Switch>
-        <Footer />
-      </>
+      <Layout className="custom-layout">
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/second">Second page</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{padding: 0}}>
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: 'trigger',
+                onClick: () => setCollapsed(!collapsed),
+              },
+            )}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}>
+            <Switch>
+              <Route path="/second" exact>
+                <Account />
+              </Route>
+              <Route path="/" exact>
+                <Content />
+              </Route>
+            </Switch>
+          </Content>
+        </Layout>
+      </Layout>
     </BrowserRouter>
   );
 };

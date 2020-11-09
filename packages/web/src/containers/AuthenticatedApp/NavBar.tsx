f1d34d7ad876fb0bd89eb383d1f9dece94847ef9
Chris Ho <dung.ho@onemount.com>
2020-11-09 23:25:53 +0700

    add more role

--- packages/web/src/containers/AuthenticatedApp/NavBar.tsx
+++ packages/web/src/containers/AuthenticatedApp/NavBar.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {DesktopOutlined, UserOutlined} from '@ant-design/icons';
+import {
+  DashboardOutlined,
+  BuildOutlined,
+  UserAddOutlined,
+  CarOutlined,
+} from '@ant-design/icons';
 import {Menu} from 'antd';
 import {Link} from 'react-router-dom';
 
@@ -10,14 +15,17 @@ const NavBar = () => {
       defaultSelectedKeys={['1']}
       mode="inline"
       collapsedWidth={50}>
-      <Menu.Item key="1" icon={<UserOutlined />}>
+      <Menu.Item key="1" icon={<DashboardOutlined />}>
         <Link to="/">Dashboard</Link>
       </Menu.Item>
-      <Menu.Item key="2" icon={<DesktopOutlined />}>
+      <Menu.Item key="2" icon={<UserAddOutlined />}>
         <Link to="/account">Quản lý tài khoản</Link>
       </Menu.Item>
-      <Menu.Item key="3" icon={<UserOutlined />}>
+      <Menu.Item key="3" icon={<CarOutlined />}>
         <Link to="/devices">Quản lý xe</Link>
+      </Menu.Item>
+      <Menu.Item key="4" icon={<BuildOutlined />}>
+        <Link to="/company">Quản lý công ty</Link>
       </Menu.Item>
     </Menu>
   );
